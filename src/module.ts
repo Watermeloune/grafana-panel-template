a99329705a5245eb7b9ec9ee9dc8e09fd7065650
Watermeloune <nersesian.robert@gmail.com>
2019-02-01 13:39:06 +0100

    editor.html added

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,32 @@
-import { PanelCtrl } from 'grafana/app/plugins/sdk'
+import { PanelCtrl , MetricsPanelCtrl} from 'grafana/app/plugins/sdk'
+import _ from 'lodash';
 import './css/style.css'
 
-class TemplatePanel extends PanelCtrl {
-  static templateUrl = "partials/module.html"
+const panelDefaults = {
+  bgColor: null
+};
+
+class TemplatePanel extends MetricsPanelCtrl {
+  static templateUrl = "partials/module.html";
+
 
   constructor($scope, $injector) {
     super($scope, $injector);
+
+    _.defaultsDeep(this.panel, panelDefaults);
     console.log("==============PanelCtrl==============");
-    console.log(PanelCtrl);
+    console.log(this);
     console.log("==============PanelCtrl==============");
+
+    this.events.on('data-received', this.onDataReceived.bind(this));
+    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+  }
+
+  onInitEditMode() {
+    this.addEditorTab('Options', 'public/plugins/beebryte-template-panel/partials/editor.html');
+  }
+
+  onDataReceived(dataList) {
 
   }
 }
